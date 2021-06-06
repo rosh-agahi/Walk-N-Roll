d0dc4870b53479d95b3434584b5e979ccfe2d9b1
rosh-agahi <rosh.agahi@gmail.com>
2021-06-06 16:09:11 -0700

    delete dog walkers

--- app/controllers/dogwalkers_controller.rb
+++ app/controllers/dogwalkers_controller.rb
@@ -4,11 +4,16 @@ class DogwalkersController < ApplicationController
     @dogwalker = Dogwalker.new
   end
 
+  def destroy
+    Dogwalker.find_by_id(params[:id]).destroy
+    redirect_to '/dogwalkers'
+  end
+
   def create
 
     @dogwalker = Dogwalker.new(dogwalker_params)
     @dogwalker.business_owner_id = helpers.current_business_owner.id
-    
+
     if @dogwalker.save
       flash[:notice] = "#{@dogwalker.name} was added as a new dog walker."
       redirect_to new_dogwalker_path
