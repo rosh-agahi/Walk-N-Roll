35e28bd4a57746f00335e099de3c28b9df95f267
rosh-agahi <rosh.agahi@gmail.com>
2021-06-06 08:13:26 -0700

    add appointments; need to add service model

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,30 @@
 class AppointmentsController < ApplicationController
+
+  def new
+    @clients = helpers.current_business_owner.clients.all
+    @dogs = Dog.all.select { |d| d.client.business_owner_id == helpers.current_business_owner.id}
+    @dogwalkers = helpers.current_business_owner.dogwalkers.all
+    @services = ["30 Minute Walk", "1 Hour Walk", "Overnight", "Include Vet Checkup"]
+    @appointment = Appointment.new
+  end
+
+  def create
+
+    @appointment = Appointment.new(appointment_params)
+
+    if @appointment.save
+      flash[:notice] = "Appointment was added for #{@appointment.dog.name} on #{@appointment.apptdate}."
+      redirect_to new_appointment_path
+    else
+      flash[:notice] = "please try again"
+      render :new
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:apptdate, :service, :dog_id, :dogwalker_id)
+  end
+
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,9 @@
 class Dog < ApplicationRecord
   belongs_to :client
   has_many :appointments
+
+  def name_with_client
+    "#{name}  (#{client.name})"
+  end
+
 end
