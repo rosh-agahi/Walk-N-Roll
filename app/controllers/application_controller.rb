3c00c9dbd43950c0040bcfa64bd17510d16bddc3
rosh-agahi <rosh.agahi@gmail.com>
2021-06-05 17:57:18 -0700

    debug create clients; set client owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  helpers do
+    def current_business_owner
+      BusinessOwner.find_by_id(session[:business_owner_id])
+    end
+
+  end
+
 end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,13 +5,21 @@ class ClientsController < ApplicationController
   end
 
   def create
+
     @client = Client.new(client_params)
+    @client.business_owner_id = session[:business_owner_id]
+
     if @client.save
-      redirect_to business_owner_path(@business_owner)
+      redirect_to business_owner_path(session[:business_owner_id])
     else
       render :new
     end
   end
 
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :phonenumber, :address)
+  end
 
 end
