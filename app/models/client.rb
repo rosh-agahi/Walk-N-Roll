8c34355238deab925ca2d2951c1ced5876eb1bb2
rosh-agahi <rosh.agahi@gmail.com>
2021-06-08 20:14:01 -0700

    clients page

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,13 @@ class Client < ApplicationRecord
   belongs_to :business_owner
   has_many :dogs
   has_many :appointments, through: :dogs
+  has_many :services, through: :appointments
+
+  def revenue
+    r = 0
+    prices = services.collect { |s| s.price }
+    rev = prices.inject(0){|r,x| r + x }
+    "$#{sprintf('%.2f',rev)}"
+  end
+
 end
