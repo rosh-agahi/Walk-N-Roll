e1504aa8f6f8de161be7ac95ac535efb63563d80
Roshanak <rosh.agahi@gmail.com>
2021-07-03 20:29:55 -0700

    adding "active" scope method to services

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,8 @@ class Service < ApplicationRecord
   belongs_to :business_owner
   has_many :appointments
 
+  scope :active, -> {where("status = ?", true)}
+
   def display_price
     "$#{sprintf('%.2f',price)}"
   end
