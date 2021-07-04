1f838d4a9fd7de248651190b81adb11db74d0bbd
Roshanak <rosh.agahi@gmail.com>
2021-07-03 20:53:07 -0700

    add validations to appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,11 @@ class Appointment < ApplicationRecord
   belongs_to :dogwalker
   belongs_to :service
 
+  validates :apptdate, presence: true, message: "Appointment must have a Date and Time."
+  validates :service_id, presence: true, message: "Please select a service for the appointment."
+  validates :dog_id, presence: true, message: "Please select the dog this appointment belongs to. "
+
+
   def display_datetime
     "#{apptdate.strftime("%A %m/%d/%Y %I:%M")} #{apptdate.strftime("%H").to_i > 12 ? ('PM') : ('AM')}"
   end
